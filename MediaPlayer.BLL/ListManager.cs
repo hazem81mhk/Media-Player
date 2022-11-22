575a90730ee36c26235189c689cd427abf9e9720
Hazem Kudaimi <59236123+hazem81mhk@users.noreply.github.com>
2022-11-22 13:59:38 +0100

    Creating ListManager class.

---
+++ MediaPlayer.BLL/ListManager.cs
@@ -0,0 +1,32 @@
+﻿// Created by: Hazem Kudaimi.
+// Date: 22/11/2022
+
+using MediaPlayer.EL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPlayer.BLL
+{
+    public class ListManager
+    {
+        private List<ShowList> showLists = new();
+        private List<ShowList> dbShowList = new();
+
+        public ListManager() { }
+
+        public List<ShowList> ShowLists
+        {
+            get{ return showLists; }
+            set { showLists = value;}
+        }
+
+        public List<ShowList> DbShowList
+        {
+            get { return dbShowList; }
+            set { dbShowList = value;}
+        }
+    }
+}
